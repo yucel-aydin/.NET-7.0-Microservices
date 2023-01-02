0ecc6e8896592b441cf7c3ec36b9017e2625579a
yucelaydin <yucel@4alabs.io>
2023-01-02 16:47:22 +0300

    Catalog Service add. Shared Project add.

---
+++ MicroservicesArchitecture/Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
